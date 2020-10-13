6872b7e7ece355d06f8d5aea3960ffcc8fb8ff2a
Vermero <julio_bermejoos@hotmail.com>
2020-10-13 17:20:51 -0300

    .

---
+++ Banco/Program.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria a conta bancaria
+            Write("Entre com o número da conta: "); int conta = int.Parse(ReadLine());
+            Write("Entre com o nome do titular da conta: "); string titular = ReadLine();
+
+            #region Deposito inicial
+            Write("\nHaverá depósito inicial? (S/N)"); string sn = ReadLine();
+            
+            bool? inicial = null;
+            double dInicial = 0;
+
+            //Altera bool
+            if (sn == "s")
+            {inicial = true;}
+            else{inicial = false;}
+            
+            if(inicial == true)
+            {Write("Entre com o valor de depósito inicial: "); dInicial = double.Parse(ReadLine());}
+
+            double saldo = 0;
+            saldo += dInicial;
+            #endregion
+
+            //Mostra dados
+            WriteLine("\n-DADOS DA CONTA-\n\nConta: " + conta + "\nTítular: " + titular + "\nSaldo: R$" + saldo);
+
+            //Deposita novo valor
+            Write("\nEntre com um valor para depósito: "); double deposito = double.Parse(ReadLine());
+            saldo += deposito;
+
+            //Mostra dados
+            WriteLine("\n-DADOS ATUALIZADOS-\n\nConta: " + conta + "\nTítular: " + titular + "\nSaldo: R$" + saldo);
+
+            //Retira valor
+            Write("\nEntre com um valor para saque: "); double saque = double.Parse(ReadLine());
+            saldo -= saque;
+
+            //Mostra dados
+            WriteLine("\n-DADOS ATUALIZADOS-\n\nConta: " + conta + "\nTítular: " + titular + "\nSaldo: R$" + saldo);
+
+
+            ReadKey();
+        }
+    }
+}
---
+++ MédiaVetor/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+using System;
+
+namespace MédiaVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Coletando dados do array
+            Write("Entre com o número de pessoas: "); 
+            int n = int.Parse(ReadLine());
+
+            //Criando array
+            double[] array = new double[n];
+
+            //Adicionando valores ao array
+            for(int i = 0; i < n; i++)
+            {
+                Write("Altura da pessoa " + n + ": ");
+                array[i] = double.Parse(ReadLine());
+            }
+
+            //Criando variavel para soma
+            double sum = 0;
+
+            //Somando todos valores do array
+            for(int i = 0; i < n; i++)
+            {
+                sum += array[i];
+            }
+
+            //Calculando e mostrando média
+            sum /= n;
+            WriteLine("A média de altura das pessoas inseridas é: " + sum);
+
+
+            
+            
+            
+            
+            ReadKey();
+        }
+    }
+}
