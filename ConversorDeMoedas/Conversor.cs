d22536df1c64b51c78f0288ab7798e52c2df5e96
Vermero <julio_bermejoos@hotmail.com>
2020-11-12 00:11:06 -0300

    .

---
+++ ConversorDeMoedas/Conversor.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using System;
+
+public class Conversor
+{
+    public float cotacao;
+    public float dolares;
+    public float reais;
+    
+    public void Conta()
+    {
+        
+        reais = dolares * cotacao;
+        
+        float iof = 0.06f * reais;
+        WriteLine("Imposto sobre Operações Financeiras: " + iof);
+
+        reais -= iof;
+
+        WriteLine("Valor a ser pago em reais: " + reais);
+    }
+}
---
+++ ConversorDeMoedas/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using System;
+
+namespace ConversorDeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor conversor = new Conversor();            
+            
+            WriteLine("Qual é a cotação do dólar?");
+            conversor.cotacao = float.Parse(ReadLine());
+
+            WriteLine("Quantos dólares você irá comprar?");
+            conversor.dolares = float.Parse(ReadLine());
+
+            conversor.Conta();
+
+
+            
+            
+            
+            ReadKey();
+        }
+    }
+}
